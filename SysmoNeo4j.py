19226a1c86660190b1f8c58788b081b37fc20206
TheMaoz <maozcyber12@gmail.com>
2022-11-22 16:13:21 +0200

    update argparse

--- SysmoNeo4j.py
+++ SysmoNeo4j.py
@@ -1,11 +1,33 @@
 import argparse
+import datetime
 
+def valid_date(s):
+    try:
+        return datetime.strptime(s, "%Y-%m-%d-%H:%M:%S")
+    except ValueError:
+        msg = "not a valid date: {0!r}".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
 def main():
-    print("Maoz")
     parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-f','--foo', help='Description for foo argument', required=True)
-    parser.add_argument('-b','--bar', help='Description for bar argument', required=True)
+    
+    parser.add_argument(
+    "-s", 
+    "--startdate", 
+    help="The Start Time - format YYYY-mm-dd-:HH:MM:SS", 
+    required=True, 
+    type=valid_date
+    )
+
+    parser.add_argument(
+    "-e", 
+    "--enddate", 
+    help="The End Time format YYYY-mm-dd-:HH:MM:SS (Inclusive)", 
+    required=True, 
+    type=valid_date
+    )
+
+    parser.add_argument('-f','--file', help='Path to json input file', required=True)
     args = vars(parser.parse_args())
 
 if __name__ == "__main__":
