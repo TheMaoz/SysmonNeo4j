b2edc28f780a906f0f66e5f489c91cdc56edf3bc
oryoav <oy7031@gmail.com>
2022-12-20 23:15:38 +0200

    remove import folder and create Cypherscripts/events

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import platform
 import shutil
 import json
 from neo4j import GraphDatabase
-
+global events_folder
+events_folder = ".\\CypherScripts\\events\\"
 
 class App:
 
     # Initializing Neo4j Driver
     def __init__(self, url, username, password):
         self.driver = GraphDatabase.driver(url, auth=(username, password))
+
 
     # Don't forget to close the driver connection when you are finished with it
     def close(self):
@@ -26,47 +28,31 @@ class App:
 
     # Processes - Event id 1 & 5.
     def upload_processes(self):
-        # Create Constraints and Indexes
-        #upload_query = """CALL apoc.cypher.runSchemaFile("UploadProcess.cypher")"""
-        #connect_query = """CALL apoc.cypher.runSchemaFile("ConnectProcessParent.cypher")"""
-        upload_query = open(f"CypherScripts/UploadProcess.cypher").read()
-        connect_query = open(f"CypherScripts/ConnectProcessParent.cypher").read()
         session = self.driver.session()
-        session.run(upload_query)
+        #                                         remove .   remove 'c:'
+        processes_json_path = (os.getcwd()+events_folder[1::])[2::]+"\\processes.json"
+        upload_query = open(f"CypherScripts/UploadProcess.cypher").read()
+        session.run(upload_query, file=processes_json_path)
+        connect_query = open(f"CypherScripts/ConnectProcessParent.cypher").read()
         session.run(connect_query)
         print("\nProcess insertion completed.")
 
-# Copy Cypher Script Schema Files to Import Path
-def copy_files_cypher_script():
-    current_path = os.getcwd()
-    current_os = platform.system()
-    if current_os == "Linux":
-        current_path += "/CypherScripts/"
-    elif current_os == "Windows":
-        current_path += "\CypherScripts\\"
-
-    shutil.copy2(current_path + "ConnectProcessParent.cypher", import_path)
-    shutil.copy2(current_path + "UploadProcess.cypher", import_path)
 
 def write_json(data, event_type):
     """
     :param data: json list of events (processes/registry/files/etc)
     :param event_type: (to defer which .json is created) processes,files...
     write data to .json in import folder.
+    :TODO: clear event_type.json if exists
     """
-    with open(".\\"+event_type + ".json", "w") as write:
+    if not os.path.exists(events_folder):
+        os.mkdir(events_folder)
+    with open(os.getcwd()+events_folder+event_type + ".json", "w") as write:
         json.dump(data, write)
 
-# Clear Import Directory
-def clear_directory():
-    for f in os.listdir(import_path):
-        os.remove(os.path.join(import_path, f))
 
-# Set Import Directory
-def set_import_path(directory):
-    global import_path
-    current_os = platform.system()
-    if current_os == "Linux":
-        import_path = directory
-    elif current_os == "Windows":
-        import_path = directory.replace("\\", "\\\\") + "\\\\"
+# Clear events Directory
+def clear_directory():
+    if os.path.exists(events_folder):
+        for f in os.listdir(events_folder):
+            os.remove(os.path.join(events_folder, f))
